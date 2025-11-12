1dee7aec2b859c2406cfc437faf2c93ad7f525ab
abdillah-9 <sabdillah855@gmail.com>
2025-11-13 01:23:12 +0300

    Third changes

--- src/pages/TopNavBar.jsx
+++ src/pages/TopNavBar.jsx
@@ -4,9 +4,9 @@ import logo from '../assets/IMG_20240513_131143_913.jpg';
 
 export default function TopNavBar() {
   return (
-    <div style={{display:'flex',flexWrap:'wrap', 
+    <div style={{display:'flex',flexWrap:'wrap', width:'100vw', 
         padding:'0px 15px',backgroundColor:'rgba(116, 80, 4, 1)',
-    justifyContent:'cente', alignItems:'center', gap:'15px', height:'120px'}}>
+    justifyContent:'cente', alignItems:'center', gap:'8px'}}>
       <LogoxName/>
       <NavLinks/>
     </div>
@@ -29,34 +29,34 @@ function NavLinks(){
     const {activeLink, setActiveLink} = useContext(NavStates);
 
     return(
-    <div style={{display:'flex', gap:"10px", flexWrap:'wrap'}}>
+    <div style={{display:'flex', gap:"8px", flexWrap:'wrap'}}>
         <span onClick={()=>{setActiveLink('Home')}} 
             style={ activeLink == 'Home' ? 
-                { cursor:'pointer',padding:"15px", ...active } : { cursor:'pointer',padding:"15px"}}
+                { cursor:'pointer',padding:"15px", ...active } : { cursor:'pointer',padding:"8px"}}
         >
             Home
         </span>
         <span onClick={()=>{setActiveLink('About Us')}}
             style={ activeLink == 'About Us' ? 
-                {  cursor:'pointer',padding:"15px", ...active } : { cursor:'pointer',padding:"15px"}}
+                {  cursor:'pointer',padding:"15px", ...active } : { cursor:'pointer',padding:"8px"}}
         >
             About Us
         </span>
         <span onClick={()=>{setActiveLink('Business Services')}}
             style={ activeLink == 'Business Services' ? 
-                {  cursor:'pointer',padding:"15px", ...active } : { cursor:'pointer',padding:"15px"}}
+                {  cursor:'pointer',padding:"15px", ...active } : { cursor:'pointer',padding:"8px"}}
         >
             Business Services
         </span>
         <span onClick={()=>{setActiveLink('Our Products')}}
             style={ activeLink == 'Our Products' ? 
-                {  cursor:'pointer',padding:"15px", ...active } : { cursor:'pointer',padding:"15px"}}
+                {  cursor:'pointer',padding:"15px", ...active } : { cursor:'pointer',padding:"8px"}}
         >
             Our Products
         </span>
         <span onClick={()=>{setActiveLink('Contacts Information')}}
             style={ activeLink == 'Contacts Information' ? 
-            {  cursor:'pointer',padding:"15px", ...active } : { cursor:'pointer',padding:"15px"}}
+            {  cursor:'pointer',padding:"15px", ...active } : { cursor:'pointer',padding:"8px"}}
         >
             Contacts Information
         </span>
